b057e19132c390787862cfd861dd7a8ad9fd2f41
SuperFromND <22881403+SuperFromND@users.noreply.github.com>
2023-10-11 18:29:58 -0500

    Disable mouse clicks when there are 0 apps

--- src/main.cpp
+++ src/main.cpp
@@ -293,7 +293,7 @@ int main(int argc, char* args[]) {
                     break;
 
                 case SDL_MOUSEBUTTONDOWN:
-                    if (x > width || x < 0 || y > height || y < 0) {break;}
+                    if (x > width || x < 0 || y > height || y < 0 || apps_count == 0) {break;}
                     if (evt.button.button == SDL_BUTTON_LEFT) {
                         SDL_DestroyRenderer(renderer);
                         SDL_DestroyWindow(window);
